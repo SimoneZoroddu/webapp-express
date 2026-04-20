f00f05bd50b7f3abf880ed83135550cabec8027e
SimoneZoroddu <lenviur@gmail.com>
2026-04-20 12:44:08 +0200

    adding review at the show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -14,14 +14,25 @@ const index = (req, res) => {
 const show = (req, res) => {
 
     const mysql = 'SELECT * FROM movies WHERE id = ?'
+    const mysqlReview = 'SELECT * FROM reviews WHERE movie_id = ? '
     const { id } = req.params
 
-    connection.query(mysql, [id], (err, results)=>{
+    connection.query(mysql, [id], (err, result)=>{
         if(err) {
             console.error(err)
             return res.status(500).json({ error: 'Database error'})
         }
-        res.json(results[0])
+
+        connection.query(mysqlReview, [id], (err, resultReview)=>{
+            if(err) {
+                console.error(err)
+                return res.status(500).json({ error: 'Database error'})
+            }
+            
+            result[0].reviews = resultReview
+
+            res.json(result[0])
+        })
     })
 }
 
